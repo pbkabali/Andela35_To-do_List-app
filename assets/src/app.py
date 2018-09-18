9664b50c295df2f55640d7ac2026a54638094598
Paul Balitema <engineerbpk@gmail.com>
2018-09-19 01:35:17 +0300

    building entry point file code

--- assets/src/app.py
+++ assets/src/app.py
@@ -6,3 +6,32 @@ if __name__ == "__main__":
     name = input("Enter your name: ")
 
     name = input("Please enter a password: ")
+
+    if login(name,password)== True:
+        return True
+    else:
+        add_account(name, password)
+
+    print("Select Option:")
+    print("1. Create Task")
+    print("2. Mark Task as Finished")
+    print("3. Delete Task")
+    print("4. Delete all Tasks")
+    print("Selection:")
+
+    selection = input("Enter your selection")
+    
+    if selection == "1":
+        task = input("Enter New Task")
+        create_task(task)
+
+    elif selection == "2":
+        task = input("Enter Task to Mark as Finished")
+        mark_as_finished(task)
+
+    elif selection == "3":
+        task = input("Enter Task to Delete")
+        delete_task(task)
+
+    elif selection == "4":
+        delete_all_tasks
